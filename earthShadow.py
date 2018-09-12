Penumbra calculation needs fixing

earthShadow.py
@@ -10,6 +10,8 @@ from astropy.coordinates import (
     Angle,
     Latitude,
     Longitude,
+    SkyCoord,
+    EarthLocation,
     solar_system_ephemeris, 
     get_body
     )
@@ -17,16 +19,21 @@ import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 
 RA_CORRECTION = Longitude(12., u.hourangle)
+CONE_ANGLE = 0.018867
 RAD_TO_DEG = 180. / np.pi
+RIGHT_ANGLE = np.pi / 2
 
 SITE_LATITUDE = 28.7603135
 SITE_LONGITUDE = -17.8796168
 SITE_ELEVATION = 2387
 
+SITE_LOCATION = EarthLocation(lat=SITE_LATITUDE*u.deg,
+                              lon=SITE_LONGITUDE*u.deg,
+                              height = SITE_ELEVATION*u.m)
+
 R_SUN = 695508000.
 R_EARTH = 6371000.
 R_GEO = 36000000.
-CONE_ANGLE = 0.018867
 
 def argParse():
     """
@@ -52,40 +59,36 @@ def argParse():
                         type=float)
     
     parser.add_argument('dec_d',
-                        help='Right ascension of target [deg]',
+                        help='Declination of target [deg]',
                         type=float)
                         
     parser.add_argument('dec_m',
-                        help='Declination og target [deg]',
+                        help='Declination of target [deg]',
                         type=float)
     
     parser.add_argument('dec_s',
-                        help='Right ascension of target [hr]',
+                        help='Declination of target [hr]',
                         type=float)
     
     return parser.parse_args()
 
 def parseInput(args):
-	"""
-	Read input epoch as an astropy Time object and input angles as 
-	astropy Angle objects
-	"""
-	
-	epoch = Time(args.epoch, format='isot', scale='utc')
-	
-	ra = Longitude((args.ra_h, args.ra_m, args.ra_s), u.hourangle)
-	dec = Latitude((args.dec_d, args,dec_m, args.dec_s), u.deg)
-	
-	return epoch, ra, dec
+    """
+    Read input epoch as an astropy Time object and input angles as 
+    astropy Angle objects
+    """
+    
+    epoch = Time(args.utc, format='isot', scale='utc')
+    
+    ra = Longitude((args.ra_h, args.ra_m, args.ra_s), u.hourangle)
+    dec = Latitude((args.dec_d, args.dec_m, args.dec_s), u.deg)
+    
+    return epoch, ra, dec
 
-def getSunEphemeris(epoch):
+def getSunEphemeris(epoch, loc=SITE_LOCATION):
     """
     Compute position of Sun relative to observer [epoch in utc]
     """
-    
-    loc = EarthLocation(lat=SITE_LATITUDE*u.deg,
-                        lon=SITE_LONGITUDE*u.deg,
-                        height = SITE_ELEVATION*u.m)
     
     sun_ephem = get_body('sun', epoch, loc)
     
@@ -103,19 +106,19 @@ def getAntiSolarPoint(ephemeris):
     
     return SkyCoord(ra=ra_asp, dec=dec_asp)
 
-def shadowRadius(altitude):
+def umbraRadius(altitude):
     """
-    Compute the radius of the inner shadow
+    Compute the radius of the inner (umbral) shadow
     """
     
     base_diam = 2. * R_EARTH
-    wing_diam = 2. * (R_GEO + R_EARTH) * np.tan(0.5 * CONE_ANGLE)
+    wing_diam = 2. * (altitude + R_EARTH) * np.tan(0.5 * CONE_ANGLE)
     
-    r_shadow = (base_diam - wing_diam) / 2.
+    r_umbra = (base_diam - wing_diam) / 4.
     
-    ang_radius = 2. * np.arctan(r_shadow / (2. * (R_GEO - SITE_ELEVATION)))
+    ang_radius = 2. * np.arctan(r_umbra / (2. * (altitude - SITE_ELEVATION)))
     
-    return ang_radius * RAD_TO_DEG / 2
+    return ang_radius * RAD_TO_DEG
 
 def penumbraRadius(d_sun):
     """
@@ -123,8 +126,7 @@ def penumbraRadius(d_sun):
     """
     
     theta = np.arctan((R_EARTH + R_SUN) / d_sun)
-    
-    d_sun_x = R_SUN * np.tan(np.pi / 2 - theta)
+    d_sun_x = R_SUN * np.tan(RIGHT_ANGLE - theta)
     
     r_penumbra = (R_EARTH + R_SUN) / d_sun * (d_sun - d_sun_x + R_EARTH + R_GEO)
     
@@ -132,43 +134,53 @@ def penumbraRadius(d_sun):
     
     return ang_radius * RAD_TO_DEG
 
-def sexagesimal(angle):
-	"""
-	Convert an angle in units of degrees to sexagesimal format
-	"""
-	
-	
-	
-	return sexagesimal_str
+def sexagesimal(angle_str):
+    """
+    Convert an angle string in degrees to a sexagesimal longitude
+    """
+    
+    angle = Angle(angle_str, u.deg)
+    sexagesimal_str = angle.to_string(u.hourangle)
+    
+    return sexagesimal_str
 
-def main(epoch, ra, dec, altitude=R_GEO):
+def main(args, altitude=R_GEO):
     """
-    Plot the Earth's inner and penumbral shadows
+    Plot Earth's umbral and penumbral shadows against target position
     """
+    
+    epoch, target_ra, target_dec = parseInput(args)
     
     sun_ephem = getSunEphemeris(epoch)
     asp = getAntiSolarPoint(sun_ephem)
     
     d_s = sun_ephem.distance
-    r_s = shadowRadius(altitude)
-    r_p = penumbraRadius(d_s)
+    r_u = umbraRadius(altitude)
+    r_p = penumbraRadius(d_s.value) 
     
     # plot the observer's target position
     fig, ax = plt.subplots()
+    
     plt.plot(sun_ephem.ra.deg, sun_ephem.dec.deg,'rx')
-    plt.plot(ra, dec, 'kx')
-    
-    plt.xlim(0, 360)
-    plt.ylim(-90,90)
-    
+    plt.plot(target_ra, target_dec, 'gx')
+    """
     # present right ascension axis in units of hour angle
+    fig.canvas.draw()
     
+    new_xlabels = []
+    old_xlabels = [item.get_text() for item in ax.get_xticklabels()]
+    for l, label in enumerate(old_xlabels):
+        print(l, label)
+        new_xlabels.append(sexagesimal(old_xlabels[l]))
     
-    c_i = Circle(xy=(asp.ra.deg,asp.dec.deg), radius=r_s, 
+    ax.set_xticklabels(new_xlabels) 
+    """
+    # add circles to represent umbral and penumbral shadows
+    c_u = Circle(xy=(asp.ra.deg,asp.dec.deg), radius=r_u, 
                facecolor="black", edgecolor='black', alpha=0.8)
-    c_i.set_facecolor('black')
-    c_i.set_edgecolor('black')
-    ax.add_artist(c_i)
+    c_u.set_facecolor('black')
+    c_u.set_edgecolor('black')
+    ax.add_artist(c_u)
     
     c_p = Circle(xy=(asp.ra.deg,asp.dec.deg), radius=r_p, 
                facecolor="black", edgecolor='black', alpha=0.6)
@@ -176,12 +188,16 @@ def main(epoch, ra, dec, altitude=R_GEO):
     c_p.set_edgecolor('black')
     ax.add_artist(c_p)
     
-    plt.grid(True)
-    
-    plt.title('Epoch: ' + epoch)
+    # final touches    
+    plt.title('Epoch: ' + str(epoch))
     
     plt.xlabel('ra [deg]')
     plt.ylabel('dec [deg]')
+    
+    plt.xlim(0, 360)
+    plt.ylim(-90,90)
+    
+    plt.grid(True)
     
     plt.show()
     plt.close(fig)
@@ -190,4 +206,4 @@ if __name__ == "__main__":
     
     args = argParse()
     
-    main(args.epoch, args.ra, args.dec)
+    main(args)